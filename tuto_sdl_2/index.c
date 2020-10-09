cc1d3e2291377646c3099ffe070057ce136f4fa1
clementreiffers <reiffersclement@gmail.com>
2020-10-09 08:50:38 +0200

    .

--- tuto_sdl_2/index.c
+++ tuto_sdl_2/index.c
@@ -1,11 +1,26 @@
-#include "SDL/SDL.h"
- 
-int main( int argc, char* args[] ) { 
-	//Demarrer SDL 
-	SDL_Init( SDL_INIT_EVERYTHING ); 
- 
-	//Quitter SDL 
-	SDL_Quit(); 
- 
-	return 0; 
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+
+int main( int argc, char *argv[ ] )
+{
+    SDL_Surface *screen;
+    if( SDL_Init( SDL_INIT_VIDEO ) == -1 )
+    {
+        printf( "Can't init SDL:  %s\n", SDL_GetError( ) );
+        return EXIT_FAILURE;
+    }
+
+    atexit( SDL_Quit ); 
+    screen = SDL_SetVideoMode( 640, 480, 16, SDL_HWSURFACE );
+
+    if( screen == NULL )
+    {
+        printf( "Can't set video mode: %s\n", SDL_GetError( ) );
+        return EXIT_FAILURE;
+    }   
+
+    SDL_Delay( 3000 );
+
+    return EXIT_SUCCESS;
 }
